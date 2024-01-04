b00e4599ca816cb94adfedc529135a37d3bef5b5
PaigeSylvan <paigesylvan@gmail.com>
2024-01-03 18:13:53 -0600

    wip

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,5 +1,10 @@
 function logger(req, res, next) {
   // DO YOUR MAGIC
+  const timestamp = new Date().toLocaleString()
+  const method = req.method
+  const url = req.originalUrl
+  console.log(`[${timestamp}] ${method} to ${url}`)
+  next()
 }
 
 function validateUserId(req, res, next) {
@@ -15,3 +20,9 @@ function validatePost(req, res, next) {
 }
 
 // do not forget to expose these functions to other modules
+module.exports = {
+  logger,
+  validateUserId,
+  validateUser,
+  validatePost,
+}
