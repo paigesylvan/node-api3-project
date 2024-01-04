29be0c9af247731273c55d786a8c70438ae571d5
PaigeSylvan <paigesylvan@gmail.com>
2024-01-03 23:16:49 -0600

    done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // require your server and launch it
 const server = require('./api/server')
-const PORT = 9000
+const PORT = 5000
 
 server.listen(PORT, () => {
     console.log('listening on', PORT)
